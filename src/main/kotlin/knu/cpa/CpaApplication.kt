8aada0f316ff039ed959d7e2761af026338c0f6b
Seungkyu-Han <trust1204@gmail.com>
2024-05-26 23:57:56 +0900

    start springboot

---
+++ src/main/kotlin/knu/cpa/CpaApplication.kt
@@ -0,0 +1,11 @@
+package knu.cpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CpaApplication
+
+fun main(args: Array<String>) {
+	runApplication<CpaApplication>(*args)
+}
---
+++ src/test/kotlin/knu/cpa/CpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package knu.cpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
